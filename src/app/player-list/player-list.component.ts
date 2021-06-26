60c8c5a6c5239d172ffcd51d8d65d8da69ec8101
Gleb Kosheev <78924110+GKosheev@users.noreply.github.com>
2021-06-26 16:55:35 -0400

    added player list component

---
+++ src/app/player-list/player-list.component.ts
@@ -0,0 +1,97 @@
+import {Component, AfterViewInit, ViewChild, OnInit} from '@angular/core';
+import {MatPaginator} from '@angular/material/paginator';
+import {MatSort} from '@angular/material/sort';
+import {MatTableDataSource} from '@angular/material/table';
+
+
+export interface Player {
+  name: string;
+  phone: string;
+  email: string;
+  rating: string;
+}
+
+
+const PLAYERS: Player[] = [
+  {
+    name: 'gleb',
+    phone: '-',
+    email:'-',
+    rating: '1'
+  },
+  {
+    name: 'anton',
+    phone: '-',
+    email:'-',
+    rating: '2'
+  },
+  {
+    name: 'oleg',
+    phone: '-',
+    email: '-',
+    rating: '3'
+  },
+  {
+    name: 'eneg',
+    phone: '-',
+    email:'-',
+    rating: '4'
+  },
+  {
+    name: 'sergey',
+    phone: '-',
+    email:'-',
+    rating: '5'
+  },
+  {
+    name: 'aleg',
+    phone: '-',
+    email:'-',
+    rating: '6'
+  }
+]
+
+
+@Component({
+  selector: 'app-player-list',
+  templateUrl: './player-list.component.html',
+  styleUrls: ['./player-list.component.scss']
+})
+export class PlayerListComponent implements AfterViewInit, OnInit {
+
+  constructor() {
+    this.dataLoaded = false;
+
+  }
+
+  dataLoaded: boolean;
+  displayedColumns: string[] = ['name', 'phone', 'email', 'rating'];
+  dataSource = new  MatTableDataSource<Player>(PLAYERS);
+
+  @ViewChild(MatPaginator) paginator: MatPaginator | any;
+  @ViewChild(MatSort) sort: MatSort = new MatSort();
+
+
+  ngAfterViewInit() {
+    this.dataSource.paginator = this.paginator;
+    this.dataSource.sort = this.sort;
+  }
+
+  ngOnInit() {
+    //server logic implementation
+    this.dataSource.data = PLAYERS;
+    this.dataLoaded = true;
+  }
+
+
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+
+    if (this.dataSource.paginator) {
+      this.dataSource.paginator.firstPage();
+    }
+  }
+}
+
+
