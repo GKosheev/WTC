450866d84ab6a42093213c48654216ff53af1a50
Gleb Kosheev <78924110+GKosheev@users.noreply.github.com>
2021-08-04 16:23:29 -0400

    added iprofile and iprofile-edit interfaces

---
+++ src/app/shared/interfaces/iprofile-edit.ts
@@ -0,0 +1,10 @@
+export interface IProfileEdit {
+  fullName: string;
+  email: string;
+  twitter: string;
+  instagram: string;
+  facebook: string;
+  receiveClubEmails: boolean;
+  shareMyEmail: boolean;
+  rating: number;
+}
---
+++ src/app/shared/interfaces/iprofile.ts
@@ -0,0 +1,15 @@
+export interface IProfile {
+  fullName: string;
+  email: string;
+  registerType: string;
+  phone: string;
+  gender: string;
+  dateOfBirth: Date;
+  memberID: string;
+  twitter: string;
+  instagram: string;
+  facebook: string;
+  receiveClubEmails: boolean;
+  shareMyEmail: boolean;
+  rating: number;
+}
