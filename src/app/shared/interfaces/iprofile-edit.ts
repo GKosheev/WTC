5b7118fa7888c41d5eaa510198bbdb5b475be87f
Gleb Kosheev <78924110+GKosheev@users.noreply.github.com>
2021-08-04 16:34:42 -0400

    fullName->firstName, lastName

--- src/app/shared/interfaces/iprofile-edit.ts
+++ src/app/shared/interfaces/iprofile-edit.ts
@@ -1,5 +1,6 @@
 export interface IProfileEdit {
-  fullName: string;
+  firstName: string;
+  lastName: string;
   email: string;
   twitter: string;
   instagram: string;
--- src/app/shared/interfaces/iprofile.ts
+++ src/app/shared/interfaces/iprofile.ts
@@ -1,5 +1,6 @@
 export interface IProfile {
-  fullName: string;
+  firstName: string;
+  lastName: string;
   email: string;
   registerType: string;
   phone: string;
