06fcc0b20a9c0480810e96e5cddf92698c0f073a
Gleb Kosheev <78924110+GKosheev@users.noreply.github.com>
2021-08-07 10:50:16 -0400

    changed user and iprofile interfaces

--- src/app/shared/interfaces/iprofile.ts
+++ src/app/shared/interfaces/iprofile.ts
@@ -2,15 +2,15 @@ export interface IProfile {
   firstName: string;
   lastName: string;
   email: string;
-  registerType: string;
+  registrationType: string;
   phone: string;
   gender: string;
   dateOfBirth: Date;
+  receiveClubEmails: boolean;
   memberID: string;
   twitter: string;
   instagram: string;
   facebook: string;
-  receiveClubEmails: boolean;
   shareMyEmail: boolean;
   rating: number;
 }
