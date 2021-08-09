ddc90d92795495cf71c511955e862696712c43e6
Gleb Kosheev <78924110+GKosheev@users.noreply.github.com>
2021-08-08 20:02:19 -0400

    changed a way of showing data in profile-edit component

--- src/app/tennis/components/profile-edit/profile-edit.component.ts
+++ src/app/tennis/components/profile-edit/profile-edit.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {IProfile} from "../../../shared/interfaces/iprofile";
 import {ProfileService} from "../../../shared/services/profiles/profile.service";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-profile-edit',
@@ -9,18 +10,32 @@ import {ProfileService} from "../../../shared/services/profiles/profile.service"
 })
 export class ProfileEditComponent implements OnInit {
   profile: IProfile = {} as IProfile
+  firstName: string = '';
+  lastName: string = '';
+  twitter: string = '';
+  instagram: string = '';
+  facebook: string = '';
+  constructor(private profileService: ProfileService, private router: Router) {
 
-  constructor(private profileService: ProfileService) { }
+  }
 
   ngOnInit(): void {
     this.profileService.getUserProfile().subscribe(user => {
       if (user !== undefined) {
         this.profile = user
+        this.firstName = this.profile.firstName
+        this.lastName = this.profile.lastName
+        this.twitter = this.profile.twitter
+        this.instagram = this.profile.instagram
+        this.facebook = this.profile.facebook
       }
     })
   }
-  saveChanges(): void{
-   // console.log("changed data: " + JSON.stringify(this.profile))
-    this.profileService.saveChanges(this.profile)
+
+  saveChanges(): void {
+    // TODO validation if (profile === profile$), in order not to call post method if data hasn't been changed
+    this.profileService.saveChanges(this.profile, this.profileService.id).subscribe(data =>{
+      return this.router.navigateByUrl('/profile')
+    })
   }
 }
