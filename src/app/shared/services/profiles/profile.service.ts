f7604fa81e1094c2a4d3754ee196042859332b8c
Gleb Kosheev <78924110+GKosheev@users.noreply.github.com>
2021-08-08 20:09:53 -0400

    Refactoring, added api for editing profile data

--- src/app/shared/services/profiles/profile.service.ts
+++ src/app/shared/services/profiles/profile.service.ts
@@ -1,44 +1,50 @@
 import {Injectable} from '@angular/core';
 import {BehaviorSubject, Observable} from "rxjs";
 import {IProfile} from "../../interfaces/iprofile";
-import {IProfileEdit} from "../../interfaces/iprofile-edit";
 import {HttpClient} from "@angular/common/http";
-import {pluck, tap} from "rxjs/operators";
 import {AuthService} from "../auth/auth.service";
-import {Router} from "@angular/router";
+
+interface IEditProfileResponse {
+  message?: string;
+  error?: any;
+}
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProfileService {
-  private USER_PROFILE_URL = '';
-  private SAVE_CHANGES_URL = '';
+  private SAVE_CHANGES_URL = 'http://localhost:5000/api/profile/edit-profile';
+  private userID: string = '';
+  private profile$ = new BehaviorSubject<IProfile | undefined>(undefined)
 
-  private user$ = new BehaviorSubject<IProfile | undefined>(undefined)
+  constructor(private http: HttpClient, private auth: AuthService) {
+  }
 
-  constructor(private router: Router, private http: HttpClient, private auth: AuthService) {
+  get id() {
+    return this.userID
   }
 
   loadUserProfile(): void {
     this.auth.getUser()
       .subscribe(user => {
         this.setUserProfile(user?.profile)
+        if (user?._id !== undefined) {
+          this.userID = user?._id
+        }
       })
-
   }
 
   setUserProfile(user: IProfile | undefined): void {
-    this.user$.next(user)
+    this.profile$.next(user)
   }
 
   getUserProfile(): Observable<IProfile | undefined> {
     this.loadUserProfile()
-    return this.user$.asObservable()
+    return this.profile$.asObservable()
   }
 
-  saveChanges(user: IProfile | undefined): void {
-    // TODO
-    this.router.navigateByUrl('/profile')
+  saveChanges(profile: IProfile | undefined, userID: string): Observable<IEditProfileResponse> {
+    return this.http.post<IEditProfileResponse>(this.SAVE_CHANGES_URL, {id: userID, profile: profile})
   }
 
 }
