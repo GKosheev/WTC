46bbaea941412080aaa0a8df551d7c2647948bb4
Gleb Kosheev <78924110+GKosheev@users.noreply.github.com>
2021-08-22 13:05:26 -0400

    Fixed dir for loading built project

--- server/controllers/home.controller.ts
+++ server/controllers/home.controller.ts
@@ -2,5 +2,5 @@ import path from 'path'
 import {Request, Response} from 'express'
 
 module.exports.home = function (req: Request, res: Response) {
-  res.sendFile(path.join(__dirname + '../../../wtc/index.html'));
+  res.sendFile(path.join(__dirname + '../../../../../dist/wtc/index.html'));
 }
