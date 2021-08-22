dca57b9090ee4dd4c6dd06c544f3ffeb1f0e480e
Gleb Kosheev <78924110+GKosheev@users.noreply.github.com>
2021-08-22 17:42:42 -0400

    Refactoring, updated loadUserData function

--- src/app/shared/services/player list/table.service.ts
+++ src/app/shared/services/player list/table.service.ts
@@ -1,8 +1,9 @@
 import {Injectable} from '@angular/core';
 import {HttpClient} from "@angular/common/http";
 import {BehaviorSubject, Observable} from "rxjs";
-import {ITableData} from "../../interfaces/i-table-data";
+import {TableData} from "../../interfaces/table/table.data.interface";
 import {tap} from "rxjs/operators";
+import {UserProfile} from "../../interfaces/table/user.profile.interface";
 
 
 @Injectable({
@@ -10,25 +11,30 @@ import {tap} from "rxjs/operators";
 })
 export class TableService {
   private GET_USERS_URL = 'http://localhost:5000/api/table/users';
-  private users$ = new BehaviorSubject<ITableData[] | undefined>(undefined)
+  private GET_USER_PROFILE_URL = 'http://localhost:5000/api/table/'
+  private users$ = new BehaviorSubject<TableData[] | undefined>(undefined)
 
   constructor(private http: HttpClient) {
   }
 
 
-  loadTableData(): Observable<ITableData[]> {
-    return this.http.get<ITableData[]>(this.GET_USERS_URL)
-     /* // if data will be needed more than in 1 component
-      .pipe(
-      tap(data => {
-        this.setTableData(data)
-      })
-    )
-      */
+  loadTableData(): Observable<TableData[]> {
+    return this.http.get<TableData[]>(this.GET_USERS_URL)
+    /* // if data will be needed more than in 1 component
+     .pipe(
+     tap(data => {
+       this.setTableData(data)
+     })
+   )
+     */
   }
 
-  setTableData(data: ITableData[]): void {
+  setTableData(data: TableData[]): void {
     this.users$.next(data)
+  }
+
+  loadUserData(id: string): Observable<UserProfile> {
+    return this.http.get<UserProfile>(this.GET_USER_PROFILE_URL + id)
   }
 
 }
