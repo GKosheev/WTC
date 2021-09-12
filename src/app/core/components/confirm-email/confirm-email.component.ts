79e253a8791360fc2216158696ce8fad79109282
Gleb Kosheev <78924110+GKosheev@users.noreply.github.com>
2021-09-12 11:09:12 -0400

    Updated confirm-email.component
    - Added login, signup and resendLink variables
    - Implemented resend function for resending user email
    - Mat-spinner shows when data is not yet loaded.

--- src/app/core/components/confirm-email/confirm-email.component.ts
+++ src/app/core/components/confirm-email/confirm-email.component.ts
@@ -9,22 +9,72 @@ import {ActivatedRoute} from "@angular/router";
 })
 export class ConfirmEmailComponent implements OnInit {
 
-  responseMessage: string = ''; // message we get after confirming email
+  responseMessage: string; // message we get after confirming email
+  login: boolean;
+  signup: boolean;
+  resendLink: boolean;
+
+  dataLoaded: boolean; // waiting for the response from server
+
   constructor(private auth: AuthService, private route: ActivatedRoute) {
+    this.login = false;
+    this.signup = false;
+    this.resendLink = false;
+    this.dataLoaded = false;
+    this.responseMessage = '';
   }
 
   ngOnInit(): void {
     this.route.params.subscribe(params => {
       this.auth.confirmEmail(params['email'], params['token']).subscribe(response => {
-          if (response.message)
+          if (response.message) {
             this.responseMessage = response.message
-          else
+            this.login = true
+            this.dataLoaded = true
+          } else
             this.responseMessage = 'server error'
         },
         error => {
-            this.responseMessage = error.error.error
+          console.log(JSON.stringify(error))
+          this.checkResponseErrors(error)
+          this.responseMessage = error.error.error
+          this.dataLoaded = true;
         })
     })
   }
 
+  resetAllVariables(): void {
+    this.login = false;
+    this.signup = false;
+    this.resendLink = false;
+    this.dataLoaded = false;
+    this.responseMessage = '';
+  }
+
+  checkResponseErrors(error: any): void {
+    if (error.error.signup)
+      this.signup = true
+
+    else if (error.error.login)
+      this.login = true
+
+    else if (error.error.resendLink)
+      this.resendLink = true;
+  }
+
+  resend(): void {
+    this.resetAllVariables()
+    this.route.params.subscribe(params => {
+      this.auth.resendLink(params['email']).subscribe(response => {
+          if (response.message)
+            this.responseMessage = response.message
+          this.dataLoaded = true;
+        },
+        error => {
+          this.checkResponseErrors(error)
+          this.responseMessage = error.error.error
+          this.dataLoaded = true;
+        })
+    })
+  }
 }
