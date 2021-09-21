2309bb005dcdebc0f6ca9dd708e7e7d3e9dff77d
Gleb Kosheev <78924110+GKosheev@users.noreply.github.com>
2021-09-21 16:08:25 -0400

    Fixed error in config/mongoose.ts

--- server/config/mongoose.ts
+++ server/config/mongoose.ts
@@ -10,7 +10,7 @@ async function startDB(): Promise<void> {
       useCreateIndex: true
     })
   } catch (error) {
-    console.log('Mongoose startDB error: ', error.message)
+    console.log('Mongoose startDB error: ', error)
     process.exit(1)
   }
 }
