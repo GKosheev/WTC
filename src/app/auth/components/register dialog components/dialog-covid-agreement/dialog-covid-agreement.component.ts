c16b92b9cb6214265496ff986ce8eb7e6785f0da
freezbek <toxa-n99@mail.ru>
2021-11-08 01:05:16 +0300

    updated frontend

--- src/app/auth/components/register dialog components/dialog-covid-agreement/dialog-covid-agreement.component.ts
+++ src/app/auth/components/register dialog components/dialog-covid-agreement/dialog-covid-agreement.component.ts
@@ -1,6 +1,6 @@
-import {Component, Inject, OnInit} from '@angular/core';
-import {MAT_DIALOG_DATA, MatDialogRef} from "@angular/material/dialog";
-import {DialogFirstAgreementComponent} from "../dialog-first-agreement/dialog-first-agreement.component";
+import { Component, Inject, OnInit } from '@angular/core';
+import { MAT_DIALOG_DATA, MatDialogRef } from "@angular/material/dialog";
+import { DialogFirstAgreementComponent } from "../dialog-first-agreement/dialog-first-agreement.component";
 
 @Component({
   selector: 'app-dialog-covid-agreement',
@@ -11,7 +11,7 @@ export class DialogCovidAgreementComponent implements OnInit {
 
   constructor(public thisDialogRef: MatDialogRef<DialogFirstAgreementComponent>, @Inject(MAT_DIALOG_DATA) public data: string) { }
 
-  ngOnInit(): void {}
+  ngOnInit(): void { }
 
   onCloseConfirm() {
     this.thisDialogRef.close('Confirm');
