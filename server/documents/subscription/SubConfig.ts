c64607838a6032292350560c4a87e6448d4eb4ba
Gleb Kosheev <78924110+GKosheev@users.noreply.github.com>
2021-12-12 13:02:33 -0500

    Updated subConfig document & model

--- server/documents/subscription/SubConfig.ts
+++ server/documents/subscription/SubConfig.ts
@@ -1,16 +1,17 @@
 import mongoose from "mongoose"
 
 export type SubConfig = mongoose.Document & {
-    _id: mongoose.Schema.Types.ObjectId,
-    subType: string,
-    images: string[],
-    subStart: string,
-    subEnd: string,
-    subscriptions: SubConfigType[]
+  _id: mongoose.Schema.Types.ObjectId,
+  subType: string,
+  images: string[],
+  subStart: string,
+  subEnd: string,
+  subscriptions: SubConfigType[]
 }
 
 export interface SubConfigType {
-    name: string,
-    price: number,
-    description: string
+  _id: mongoose.Schema.Types.ObjectId,
+  name: string,
+  price: number,
+  description: string
 }
--- server/models/subscription/sub-config.model.ts
+++ server/models/subscription/sub-config.model.ts
@@ -1,17 +1,19 @@
 import mongoose from "mongoose";
-import {SubConfig} from "../../documents/subscription/SubConfig";
+import {SubConfig, SubConfigType} from "../../documents/subscription/SubConfig";
 
+
+const subscriptionSchema = new mongoose.Schema<SubConfigType>({
+  name: {type: String, required: true},
+  price: {type: Number, required: true},
+  description: {type: String}
+})
 
 const subConfigSchema = new mongoose.Schema<SubConfig>({
-    subType: {type: String, required: true, unique: true},
-    images: [{type: String}],
-    subStart: {type: String, required: true},
-    subEnd: {type: String, required: true},
-    subscriptions: [{
-        name: {type: String, required: true, unique: true},
-        price: {type: Number, required: true},
-        description: {type: String}
-    }]
+  subType: {type: String, required: true, unique: true},
+  images: [{type: String}],
+  subStart: {type: String, required: true},
+  subEnd: {type: String, required: true},
+  subscriptions: [{type: subscriptionSchema, required: true}]
 })
 
 const SubConfigModel = mongoose.model<SubConfig>('Sub_Config', subConfigSchema)
