129628a921a13b25f418a16ff83d8233cef6431f
Gleb Kosheev <78924110+GKosheev@users.noreply.github.com>
2021-12-16 13:47:48 -0500

    Implemented courts-config.model.ts

---
+++ server/models/courts/courts-config.model.ts
@@ -0,0 +1,20 @@
+import mongoose from "mongoose";
+import {CourtsConfig} from "../../documents/courts/CourtsConfig";
+
+
+const courtsConfigSchema = new mongoose.Schema<CourtsConfig>({
+  description: {type: String, default: '-'},
+  images: [{type: String}],
+  courtType: {type: String, required: true, unique: true},
+  courts: {
+    courtId: {type: Number, required: true}, // unique: true?
+    time: [{type: String, required: true}],
+  },
+  priceConfigs: {
+    subType: {type: String, required: true},
+    price: {type: String, required: true}
+  }
+})
+
+const CourtsConfigModel = mongoose.model<CourtsConfig>('Courts_Config', courtsConfigSchema)
+export default CourtsConfigModel
