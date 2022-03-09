b0c15b451cbb7629b60a56305a033b89ecb8cb6f
Gleb Kosheev <78924110+GKosheev@users.noreply.github.com>
2022-03-09 17:23:47 -0500

    Updated third image of the slider, now slider changes images automatically

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-auth',
@@ -7,9 +7,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AuthComponent implements OnInit {
 
-  constructor() { }
-  variable: boolean = false;
+
+  constructor() {
+  }
+
   ngOnInit(): void {
+    let pics = [true, false, false]
+    setInterval(function (){
+      if (pics[0])
+      {
+        pics[0] = false;
+        pics[1] = true;
+        // @ts-ignore
+        document.getElementById('1_next').click()
+
+      }
+      else if (pics[1]){
+        pics[1] = false
+        pics[2] = true
+        // @ts-ignore
+        document.getElementById('2_next').click()
+      }
+      else if (pics[2]){
+        pics[2] = false;
+        pics[1] = true;
+        // @ts-ignore
+        document.getElementById('3_next').click()
+      }
+    }, 5000)
   }
 
 }
