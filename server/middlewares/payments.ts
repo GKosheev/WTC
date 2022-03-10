948023c3b8f901dcc90906692584779f1ea8c690
Gleb Kosheev <78924110+GKosheev@users.noreply.github.com>
2022-03-09 22:08:37 -0500

    Fixed payments' middleware

--- server/middlewares/payments.ts
+++ server/middlewares/payments.ts
@@ -26,38 +26,46 @@ export async function checkoutMiddleware(req: Request, res: Response, next: Next
   const storeItems = body.storeIds!
   const courtItems = body.courtIds!
 
-  const subPayment = await SubPaymentModel.findOne({clubCardId: user.clubCardId})
-  if (!subPayment)
-    return res.status(400).json({msg: 'wrong user club card id'})
 
   const subsInfo: SubInfo[] = []
-  subPayment.subPayments.forEach(sub => {
-      if (subsList.includes(sub._id.toString()))
-        subsInfo.push(
-          {
-            paymentId: sub._id.toString(),
-            description: sub.subInfo.description,
-            images: sub.images,
-            price: sub.paymentInfo.price,
-            subName: sub.subInfo.subName
-          })
-    }
-  )
   const itemsInfo: ItemInfo[] = []
-  const storePayment = await StorePaymentModel.findOne({clubCardId: user.clubCardId})
-  if (!storePayment)
-    return res.status(400).json({msg: 'wrong user club card id'})
-  storePayment.storePayments.forEach(item => {
-    if (storeItems.includes(item._id.toString()))
-      itemsInfo.push({
-        itemName: item.itemInfo.name,
-        price: item.paymentInfo.price,
-        description: item.itemInfo.description,
-        images: item.images,
-        quantity: item.itemInfo.quantity,
-        paymentId: item._id.toString()
-      })
-  })
+
+  if (subsList.length) {
+    const subPayment = await SubPaymentModel.findOne({clubCardId: user.clubCardId})
+    if (!subPayment)
+      return res.status(400).json({msg: 'wrong user club card id'})
+
+
+    subPayment.subPayments.forEach(sub => {
+        if (subsList.includes(sub._id.toString()))
+          subsInfo.push(
+            {
+              paymentId: sub._id.toString(),
+              description: sub.subInfo.description,
+              images: sub.images,
+              price: sub.paymentInfo.price,
+              subName: sub.subInfo.subName
+            })
+      }
+    )
+  }
+
+  if (storeItems.length) {
+    const storePayment = await StorePaymentModel.findOne({clubCardId: user.clubCardId})
+    if (!storePayment)
+      return res.status(400).json({msg: 'wrong user club card id'})
+    storePayment.storePayments.forEach(item => {
+      if (storeItems.includes(item._id.toString()))
+        itemsInfo.push({
+          itemName: item.itemInfo.name,
+          price: item.paymentInfo.price,
+          description: item.itemInfo.description,
+          images: item.images,
+          quantity: item.itemInfo.quantity,
+          paymentId: item._id.toString()
+        })
+    })
+  }
 
 
   res.locals.clubCardId = user.clubCardId
